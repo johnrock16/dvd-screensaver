6961833e84c37a2ec4b9f236dc0e6ab662c9c977
João Rocha <42662031+johnrock16@users.noreply.github.com>
2024-02-25 22:20:35 -0300

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.json'
+declare module '*.ogg'
+declare module '*.wav'
+declare module '*.mp3'
+declare module '*.svg'
